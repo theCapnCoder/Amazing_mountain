d59543586c28936c7e691c6ee9b5b48c5635bbaa
PavelM <myagkiypavel@gmail.com>
2023-04-18 13:20:07 +0300

    feature: add colors and style text

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "midnight-blueberry": "#0C0E36",
+        "rosy-blaze": "#F70058",
+        "shadow-grey": "#3A3A3A",
+        "slate-lavender": "#8A8A9D",
+      },
+    },
   },
   plugins: [],
 };
